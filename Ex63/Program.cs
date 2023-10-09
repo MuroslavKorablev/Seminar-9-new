0762ebb9034de94b6c38d9823781adf1793e6aca
Kleo <mmttdd16@gmail.com>
2023-10-09 07:07:03 +0300

    Finished Ex63

--- Ex63/Program.cs
+++ Ex63/Program.cs
@@ -2,3 +2,22 @@
 // все натуральные числа в промежутке от 1 до N.
 // N = 5 -> "1, 2, 3, 4, 5"
 // N = 6 -> "1, 2, 3, 4, 5, 6"
+System.Console.WriteLine(ShowNumbers(5));
+ShowNNumbers(6);
+
+string ShowNumbers(int number)
+{
+    if (number == 1) return "1 ";
+    // else return $"{number} " + ShowNumbers(number - 1); // От N до 1
+    else return ShowNumbers(number - 1) + $"{number} "; // От 1 до N
+}
+
+void ShowNNumbers(int number)
+{
+    if (number == 0) return;
+    else
+    {
+        ShowNNumbers(number - 1);
+        Console.Write(number + " ");
+    }
+}
