ab3de1df59b0c46f929cf0a7e854cfd288cdfa98
Kleo <mmttdd16@gmail.com>
2023-10-09 20:09:52 +0300

    Create Ex67

---
+++ Ex67/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+
+ShowNNumbers(45);
+System.Console.WriteLine(SumOfDigit(453));
+void ShowNNumbers(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        int digit = number % 10;  // Получаем последнюю цифру числа
+        sum += digit;             // Прибавляем цифру к сумме
+        number /= 10;             // Удаляем последнюю цифру из числа
+    }
+    System.Console.WriteLine(sum);
+}
+
+int SumOfDigit(int num)
+{
+    if (num == 0) return num;
+    else return num % 10 + SumOfDigit(num / 10);
+}
